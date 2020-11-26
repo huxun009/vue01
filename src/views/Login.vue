76a47b01d3d3a75af0bbe3def1781429dc9f31de
17714487360 <1024877209@qq.com>
2020-11-26 22:02:17 +0800

    first commit

---
+++ src/views/Login.vue
@@ -0,0 +1,78 @@
+<template>
+<div>
+  <el-form :rules="rules" :model="loginForm" class="loginContainer" ref="loginForm">
+    <h3 class="loginTitle">系统登录</h3>
+    <el-form-item prop="username">
+      <el-input type="text" v-model="loginForm.username" auto-complete="off" placeholder="请输入用户名"></el-input>
+    </el-form-item>
+    <el-form-item prop="password">
+      <el-input type="password" v-model="loginForm.password"
+                auto-complete="off" placeholder="请输入密码" @keydown.enter.native="submitLogin"></el-input>
+    </el-form-item>
+    <el-checkbox class="loginRemeber" v-model="checked"></el-checkbox>
+    <el-button type="primary" style="width: 100%" @click="submitLogin">登录</el-button>
+  </el-form>
+</div>
+</template>
+
+<script>
+
+
+
+export default {
+name: "Login",
+  data(){
+    return {
+      loginForm:{
+        username: 'admin',
+        password: '123'
+      },
+      checked:true,
+      rules: {
+        username:[{required:true,message:'请输入用户名',trigger:'blur'}],
+        password:[{required:true,message:'请输入密码',trigger:'blur'}],
+      }
+    }
+  },
+  methods:{
+    submitLogin() {
+      this.$refs.loginForm.validate((valid) => {
+        if (valid) {
+          this.postKeyValueRequest('doLogin',this.loginForm).then(resp=>{
+            if (resp){
+              window.sessionStorage.setItem("user",JSON.stringify(resp.obj));
+              this.$router.replace('/home')
+            }
+          })
+          // alert('submit!');
+        } else {
+          this.$message.error('请输入所有字段');
+          return false;
+        }
+      });
+  }
+}
+}
+</script>
+
+<style>
+  .loginContainer{
+    border-radius: 15px;
+    background-clip: padding-box;
+    margin: 180px auto;
+    width: 350px;
+    padding: 25px 35px 15px 35px;
+    background: aliceblue;
+    border: 1px solid #b1aaee;
+    box-shadow: 0 0 25px #cac6c6;
+  }
+  .loginTitle{
+    margin: 15px auto 15px auto;
+    text-align: center;
+    color: black;
+  }
+  .loginRemeber{
+    text-align: left;
+    margin: 0px 0px 15px 0px;
+  }
+</style>
